32e33c3ad6bdcf10c535c06bc5a43d4f933ce292
teslim <tunjiheritage@gmail.com>
2025-02-16 16:29:20 +0100

    done

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
+from .models import Feature 
 # from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    context = {
-        "name": "John",
-        "age": 23,
-        "nationalities": ["American", "Canadian"]
-    }
+# def index(request):
+#     context = {
+#         "name": "John",
+#         "age": 23,
+#         "nationalities": ["American", "Canadian"]
+#     }
 
-    return render(request,"index.html", context)
-    # return render(request,"index.html", {"context":context})
-    # return HttpResponse("hello world")
+#     return render(request,"index.html", context)
+#     # return render(request,"index.html", {"context":context})
+#     # return HttpResponse("hello world")
+    
+
+
+def index(request):
+    features = Feature.objects.all()
+    
+    return render(request,"index.html",{"features":features})
